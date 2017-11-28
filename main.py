93b08ccd7669551e1cdb7cbcddf24ef326f99195
starun96 <tarunsaharya@gmail.com>
2017-11-27 20:25:06 -0500

    Finished parsing input

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-
 def create_schedule_from_input(input):
     """
     Creates a schedule of instructions from user input
     :param input: user inputted set of instructions, space-separated
     :return: a set of instructions (schedule) of the form (R/W, num, data_item)
     """
+
     instructions_text = input.split()
-    instructions = set()
+    schedule = set()
     for instruction_text in instructions_text:
-        instruction = ()
-        instructions.add(instruction)
+        sides = instruction_text.split('(')
+        read_or_write = sides[0][0]
+        number = sides[0][1:]
+        length_data_item_text = len(sides[1]) - 1
+        data_item = sides[1][:length_data_item_text]
+        instruction = (read_or_write, number, data_item)
+        schedule.add(instruction)
+
+    return schedule
 
 def is_conflict_serializable(schedule):
     pass
 
+
 def create_serial_equiv_schedule():
     pass
+
 
 def create_at_least_one_cycle():
     pass
 
 
 if __name__ == '__main__':
-    prompt = 'Pass in a set of instructions on one line, separated by a space. Examples of instructions are R1(A) and W2(B).'
-    schedule = create_schedule_from_input(input('prompt'))
+    prompt = 'Pass in a set of instructions on one line, separated by a space. Examples of instructions are R1(A) and W2(B).\n'
+    schedule = create_schedule_from_input(input(prompt))
+    print(schedule)
     is_conflict_serializable(schedule)
     create_serial_equiv_schedule()
     create_at_least_one_cycle()
